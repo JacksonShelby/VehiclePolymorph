efa06b67a89e60cc345bfe16a67ae7f48d426645
Jackson <wshelby@cub.uca.edu>
2025-07-29 14:54:06 -0500

    final

--- VehicleTester/VehicleTester.cpp
+++ VehicleTester/VehicleTester.cpp
@@ -10,25 +10,24 @@
 using namespace std;
 
 int main() {
-  // Create some Mover objects
+
   Mover moverCar(60.5);
   Mover moverTruck(45.0);
   Mover moverMotorcycle(80.2);
 
-  // Create vehicles with movers
   Car car1("Toyota", "Camry", 2020, 5, &moverCar);
-  Truck truck1("Ford", "F-150", 2018, 1200.0, &moverTruck);
+  Truck truck1("Toyota", "Tacoma", 2018, 1200.0, &moverTruck);
   Motorcycle motorcycle1("Honda", "CBR600", 2021, 600.0, &moverMotorcycle);
 
-  // Print info and how they move
-  cout << car1 << std::endl;
-  cout << truck1 << std::endl;
-  cout << motorcycle1 << std::endl;
 
-  // Call move() directly
-  cout << "Car moves: " << car1.move() << std::endl;
-  cout << "Truck moves: " << truck1.move() << std::endl;
-  cout << "Motorcycle moves: " << motorcycle1.move() << std::endl;
+  cout << car1 << endl;
+  cout << truck1 << endl;
+  cout << motorcycle1 << endl;
+
+
+  cout << "Car moves: " << car1.move() << endl;
+  cout << "Truck moves: " << truck1.move() << endl;
+  cout << "Motorcycle moves: " << motorcycle1.move() << endl;
 
   return 0;
 }
